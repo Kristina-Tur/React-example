f70c64584c154f3088952ae770f2e58f67561edd
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-06-11 15:34:20 +0300

    USEmEMO

---
+++ src/stories/React.memo.stories.tsx
@@ -0,0 +1,31 @@
+import React, {memo, useMemo, useState} from 'react';
+import {string} from "prop-types";
+
+export default {
+    component: 'users',
+};
+
+export const Users = (props: {users: Array<string>}) => {
+    console.log('Render Users')
+    return (
+        <div>
+            {props.users.map((u, i) => <div key={i}>{u}</div>)}
+        </div>
+    )
+}
+
+const UsersMemo = memo(Users)
+
+export const helpToReactMemo = () => {
+    console.log('Render help')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Kris', 'Kris', 'Kris', 'Kris'])
+
+    return (
+        <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+            {counter}
+            <UsersMemo users={users}/>
+        </>
+    )
+}
---
+++ src/stories/useMemo.stories.tsx
@@ -0,0 +1,75 @@
+import React, {memo, useMemo, useState} from 'react';
+
+export default {
+    component: 'useMemo',
+};
+
+export const useMemoExample = () => {
+    const [a, setA] = useState<number>(5)
+    const [b, setB] = useState<number>(5)
+
+    let resultA = 1
+    let resultB = 1
+
+    resultA = useMemo(() => {
+        for(let i = 1; i <= a; i++){
+            let fake = 0
+            while(fake < 1000000){
+                fake++
+                const fakeValue = Math.random()
+            }
+            resultA = resultA * i
+        }
+        return resultA
+    }, [a])
+
+
+    for(let i = 1; i <= b; i++){
+        resultB = resultB * i
+    }
+
+    return (
+        <>
+            <input value={a} onChange={(event) => setA(+event.currentTarget.value)}/>
+            <input value={b} onChange={(event) => setB(+event.currentTarget.value)}/>
+            <div>
+                Result for a: {resultA}
+            </div>
+            <div>
+                Result for b: {resultB}
+            </div>
+
+        </>
+    )
+}
+
+export const Users = (props: {users: Array<string>}) => {
+    console.log('Render Users')
+    return (
+        <div>
+            {props.users.map((u, i) => <div key={i}>{u}</div>)}
+        </div>
+    )
+}
+
+const UsersMemo = memo(Users)
+
+export const helpToReactMemo = () => {
+    console.log('Render help')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Kris', 'Vadim', 'Jo', 'Anna'])
+
+    const newArray = useMemo(() => users.filter(u => u.indexOf('a') > -1), [])
+
+    const addUser = () => {
+        setUsers([...users, 'Sveta'])
+    }
+    return (
+        <>
+            <button onClick={() => setCounter(counter + 1)}>+</button>
+            <button onClick={() => addUser()}>add user</button>
+            {counter}
+            <UsersMemo users={newArray}/>
+        </>
+    )
+}
